78dbc7a59cafc508ab0b317b4c367bb3ea39e76b
Neto Moraes <jomoraesneto@gmail.com>
2024-04-02 18:11:25 -0300

    feat: add map feature

--- frontend/src/app/pages/local/page.tsx
+++ frontend/src/app/pages/local/page.tsx
@@ -1,49 +1,23 @@
-'use client';
 import { Suspense } from "react"
 import Loading from '../../loading'
 import styles from './styles.module.sass'
-import React, { useRef, useEffect } from 'react';
-import Map from 'ol/Map';
-import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
-import 'ol/ol.css';
-
-const Map1 = ({setMap1Object}) => {
-  const map1Container = useRef();
-
-    useEffect(() => {
-      const map1 = new Map({
-        layers: [
-          new TileLayer({
-            source: new OSM(),
-          }),
-        ],
-        view: new View({
-
-          center: [8546575.886939, 2137169.681579],
-          zoom: 6
-        }),
-      });
-      map1.setTarget(map1Container.current);
-      setMap1Object(map1);
-
-      return () => {
-        map1.setTarget(undefined);
-        setMap1Object(null);
-      };
-    }, []);
-    return (<><div ref={map1Container} className="absolute inset-0"></div></>);
-  };
-  
+import Head from "next/head"
+import Map from "@/components/MapComponents/Map"
 
 const local = () => {
   return(
-    <Suspense fallback={<Loading />}>
-      <div className={styles.container}>
-        
-      </div>
-    </Suspense>
+    <>
+      <Head>
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossOrigin=""/>
+      </Head>
+      <Suspense fallback={<Loading />}>
+        <div className={styles.container}>
+          <Map
+           position={[0 , 0]}
+          />
+        </div>
+      </Suspense>
+    </>
   )
 }
 
