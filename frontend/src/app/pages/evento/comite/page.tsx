14bcf63c2a97337027de66ff92cf689e0fef9edd
Neto Moraes <jomoraesneto@gmail.com>
2024-04-02 19:39:39 -0300

    feat: add comite and cronograma

--- frontend/src/app/pages/evento/comite/page.tsx
+++ frontend/src/app/pages/evento/comite/page.tsx
@@ -1,11 +1,55 @@
 import { Suspense } from "react"
 import Loading from "../../../loading"
+import styles from "./styles.module.sass"
+import person from "../../../../../public/assets/images/person.jpg"
+import Image from 'next/image'
+
 
 const comite = () => {
 
   return (
     <Suspense fallback={<Loading />}>
-      <h1>Comitê</h1>
+      <div className={styles.container}>
+        <section>
+          <div>
+            <Image src={person} alt="person"/>
+            <h2>Pessoa 1</h2>
+            <p>Professor do dcm</p>
+          </div>
+
+          <div>
+            <Image src={person} alt="person"/>
+            <h2>Pessoa 2</h2>
+            <p>Professor do dcm</p>
+          </div>
+
+          <div>
+            <Image src={person} alt="person"/>
+            <h2>Pessoa 3</h2>
+            <p>Professor do dcm</p>
+          </div>
+
+          <div>
+            <Image src={person} alt="person"/>
+            <h2>Pessoa 4</h2>
+            <p>Professor do dcm</p>
+          </div>
+
+          <div>
+            <Image src={person} alt="person"/>
+            <h2>Pessoa 5</h2>
+            <p>Professor do dcm</p>
+          </div>
+
+          <div>
+            <Image src={person} alt="person"/>
+            <h2>Pessoa 6</h2>
+            <p>Professor do dcm</p>
+          </div>
+
+
+        </section>
+      </div>
     </Suspense>
   )
 }
--- frontend/src/app/pages/evento/cronograma/page.tsx
+++ frontend/src/app/pages/evento/cronograma/page.tsx
@@ -6,38 +6,49 @@ const cronograma = () => {
   return(
     <Suspense fallback={<Loading />}>
       <div className={styles.container}>
-        <h1>Cronograma</h1>
+        <section>
+          <h1>Cronograma</h1>
 
-        <h2>Segunda-Feira</h2>
-        <p>08:00 às 10:00  -  Palestra sobre Matemática</p>
-        <p>10:00 às 12:00  -  Mesa Redonda: Cálculo Aplicado</p>
-        <p>14:00 às 16:00  -  Palestra sobre Matemática Aplicada</p>
-        <p>16:00 às 18:00  -  Coffee Break</p>
+          <div>
+            <h2>Segunda-Feira</h2>
+            <p>08:00 às 10:00  -  Palestra sobre Matemática</p>
+            <p>10:00 às 12:00  -  Mesa Redonda: Cálculo Aplicado</p>
+            <p>14:00 às 16:00  -  Palestra sobre Matemática Aplicada</p>
+            <p>16:00 às 18:00  -  Coffee Break</p>
+          </div>
 
-        <h2>Terça-Feira</h2>
-        <p>08:00 às 10:00  -  Palestra sobre Matemática</p>
-        <p>10:00 às 12:00  -  Mesa Redonda: Cálculo Aplicado</p>
-        <p>14:00 às 16:00  -  Palestra sobre Matemática Aplicada</p>
-        <p>16:00 às 18:00  -  Coffee Break</p>
+          <div>
+            <h2>Terça-Feira</h2>
+            <p>08:00 às 10:00  -  Palestra sobre Matemática</p>
+            <p>10:00 às 12:00  -  Mesa Redonda: Cálculo Aplicado</p>
+            <p>14:00 às 16:00  -  Palestra sobre Matemática Aplicada</p>
+            <p>16:00 às 18:00  -  Coffee Break</p>
+          </div>
 
-        <h2>Quarta-Feira</h2>
-        <p>08:00 às 10:00  -  Palestra sobre Matemática</p>
-        <p>10:00 às 12:00  -  Mesa Redonda: Cálculo Aplicado</p>
-        <p>14:00 às 16:00  -  Palestra sobre Matemática Aplicada</p>
-        <p>16:00 às 18:00  -  Coffee Break</p>
+          <div>
+            <h2>Quarta-Feira</h2>
+            <p>08:00 às 10:00  -  Palestra sobre Matemática</p>
+            <p>10:00 às 12:00  -  Mesa Redonda: Cálculo Aplicado</p>
+            <p>14:00 às 16:00  -  Palestra sobre Matemática Aplicada</p>
+            <p>16:00 às 18:00  -  Coffee Break</p>
+          </div>
 
-        <h2>Quinta-Feira</h2>
-        <p>08:00 às 10:00  -  Palestra sobre Matemática</p>
-        <p>10:00 às 12:00  -  Mesa Redonda: Cálculo Aplicado</p>
-        <p>14:00 às 16:00  -  Palestra sobre Matemática Aplicada</p>
-        <p>16:00 às 18:00  -  Coffee Break</p>
+          <div>
+            <h2>Quinta-Feira</h2>
+            <p>08:00 às 10:00  -  Palestra sobre Matemática</p>
+            <p>10:00 às 12:00  -  Mesa Redonda: Cálculo Aplicado</p>
+            <p>14:00 às 16:00  -  Palestra sobre Matemática Aplicada</p>
+            <p>16:00 às 18:00  -  Coffee Break</p>
+          </div>
 
-        <h2>Sexta-Feira</h2>
-        <p>08:00 às 10:00  -  Palestra sobre Matemática</p>
-        <p>10:00 às 12:00  -  Mesa Redonda: Cálculo Aplicado</p>
-        <p>14:00 às 16:00  -  Palestra sobre Matemática Aplicada</p>
-        <p>16:00 às 18:00  -  Coffee Break</p>
-
+          <div>
+            <h2>Sexta-Feira</h2>
+            <p>08:00 às 10:00  -  Palestra sobre Matemática</p>
+            <p>10:00 às 12:00  -  Mesa Redonda: Cálculo Aplicado</p>
+            <p>14:00 às 16:00  -  Palestra sobre Matemática Aplicada</p>
+            <p>16:00 às 18:00  -  Coffee Break</p>
+          </div>  
+        </section>
       </div>
     </Suspense>
   )
