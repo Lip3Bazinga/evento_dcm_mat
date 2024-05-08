88216db815f6dfad58dec3e51c52677c3f53d654
Filipe Magalhães <filipepio2005@hotmail.com>
2024-05-08 14:17:28 -0300

    create form

--- src/app/pages/inscricoes/page.tsx
+++ src/app/pages/inscricoes/page.tsx
@@ -4,16 +4,29 @@ import { Suspense } from "react"
 import Loading from "../../loading"
 import styles from './styles.module.sass'
 
+const GoogleForm = () => {
+  return (
+    <iframe
+      src="https://docs.google.com/forms/d/e/1FAIpQLSercQzi1Q8n8vCvmKeg0gftg_PGP5U0ukotPiJjp1VU-gPtpw/viewform?embedded=true"
+      width={640}
+      height={2433}
+      frameBorder={0}
+      marginHeight={0}
+      marginWidth={0}
+    >
+      Loading…
+    </iframe>
+
+  );
+};
+
+
 const Inscricoes = () => {
 
   return(
     <Suspense fallback={<Loading />}>
       <main className={styles.main}>
-        <h1>Inscrições</h1>
-        <p>Para garantir a sua vaga no evento, não perca tempo e se inscreva acessando o nosso formulário online!</p>
-        <a href="https://forms.gle/1mjvjYsvx7PXAKKY8" target="_blank">
-          Faça sua inscrição
-        </a>
+        <GoogleForm />
       </main>
     </Suspense>
   )
