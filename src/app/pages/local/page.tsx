5e29608b17c4d863e1457058f940aa6a025bd83e
Filipe Magalhães <filipepio2005@hotmail.com>
2024-05-09 20:24:33 -0300

    General Updates

--- src/app/pages/local/page.tsx
+++ src/app/pages/local/page.tsx
@@ -1,7 +1,7 @@
 import Map from "../../../components/maps/map"
 import Image from 'next/image'
 import av_cafe from "../../../../public/assets/images/av_cafe.jpg"
-import av_bandeirantes from "../../../../public/assets/images/av_bandeirantes.jpg"
+import av_b from "../../../../public/assets/images/av_bandeirantes.jpg"
 import styles from "./styles.module.sass"
 
 export default function Local() {
@@ -16,7 +16,7 @@ export default function Local() {
           <p>Praça Fritz Koberle, 121 - Vila Monte Alegre, Ribeirão Preto - SP, 14051-099</p>
         </div>
         <div className={styles.contentImage}>
-          <Image src={av_bandeirantes} alt="." />
+          <Image src={av_b} alt="." />
           <p>Av. Bandeirantes, 2516 - Campus da Usp, Ribeirão Preto - SP, 14040-900</p>
         </div>
       </div>
