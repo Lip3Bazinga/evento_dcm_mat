0ef9dca405b9df5d703a36bb11d7523d5f6a7454
Filipe Magalhães <filipepio2005@hotmail.com>
2024-05-09 20:17:39 -0300

    Header update and Cartaz Update

--- src/app/pages/evento/cartaz/page.tsx
+++ src/app/pages/evento/cartaz/page.tsx
@@ -1,27 +1,21 @@
-import { Suspense } from "react"
-import Loading from "../../../loading"
-import styles from "./styles.module.sass"
-import Image from "next/image"
-import preview from "../../../../../public/assets/images/preview.png"
+import { Suspense } from "react";
+import Loading from "../../../loading";
+import styles from "./styles.module.sass";
+import Image from "next/image";
+import preview from "../../../../../public/assets/images/preview.png";
+// import cartazPDF from "../../../../../public/cartaz.pdf"; // Importe o arquivo PDF
 
-const cartaz = () => {
-
-  return(
+const Cartaz = () => {
+  return (
     <Suspense fallback={<Loading />}>
-
       <main className={styles.container}>
-
         <h1>Baixar PDF do cartaz</h1>
-
-        <a href="./cartaz.pdf" download="cartaz.pdf">
+        <a href="/cartaz.pdf" download="cartaz.pdf">
           <Image src={preview} alt="Amostra do cartaz do evento" />
         </a>
-
       </main>
-
     </Suspense>
-  )
+  );
+};
 
-}
-
-export default cartaz
+export default Cartaz;
--- src/components/UI/header/index.tsx
+++ src/components/UI/header/index.tsx
@@ -1,61 +1,58 @@
-'use client'
+"use client"
 
-import Link from 'next/link'
-import styles from './styles.module.sass'
-import { useState, useEffect } from 'react'
-import open_menu_icon from '@/../public/assets/images/menu-outline.svg'
-import close_menu_icon from '@/../public/assets/images/close-outline.svg'
-import Image from 'next/image'
+import Link from 'next/link';
+import styles from './styles.module.sass';
+import { useState, useEffect } from 'react';
+import open_menu_icon from '@/../public/assets/images/menu-outline.svg';
+import close_menu_icon from '@/../public/assets/images/close-outline.svg';
+import Image from 'next/image';
 
 const Header = () => {
-  const [isOpenMenu, setIsOpenMenu] = useState(false)
-  const [isScrolled, setIsScrolled] = useState(false)
-  const [isOpenLink, setIsOpenLink] = useState(true)
-  const [isSelectedLink, setIsSelectedLink] = useState(true)
-
-  useEffect(() => {
-    // Adiciona um event listener para o evento de rolagem quando o componente monta
-    window.addEventListener('scroll', handleScroll)
-    // Remove o event listener quando o componente desmonta para evitar vazamentos de memória
-    return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
-  }, [])
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isOpenLink, setIsOpenLink] = useState(false);
 
   const handleScroll = () => {
-    const scrolled = window.scrollY > 0
-    setIsScrolled(scrolled)
-  }
+    const scrolled = window.scrollY > 0;
+    setIsScrolled(scrolled);
+  };
 
-  const headerClassName = isScrolled ? `${styles.header} ${styles.header_scroll}` : styles.header
+  const toggleMenu = () => {
+    setIsOpenMenu(!isOpenMenu);
+  };
+
+  const toggleSubMenu = () => {
+    setIsOpenLink(!isOpenLink);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const headerClassName = isScrolled ? `${styles.header} ${styles.header_scroll}` : styles.header;
 
   return (
     <header className={headerClassName}>
       <nav>
-        <button onClick={() => setIsOpenMenu(!isOpenMenu)}>
+        <button onClick={toggleMenu}>
           <Image src={isOpenMenu ? close_menu_icon : open_menu_icon} alt='Ícone para alternar menu' />
         </button>
         <ul className={isOpenMenu ? styles.active : ''}>
           <li><Link href="/" className={styles.link}>Home</Link></li>
-          <li onClick={() => setIsOpenLink(!isOpenLink)}>
-            {isOpenLink ? 
-              (
-                <Link href="#"  className={styles.link}>O evento</Link>
-              )
-            : 
-              (
-                <>
-                <Link href="#" className={styles.link}>O evento</Link>
+          <li onMouseEnter={toggleSubMenu} onMouseLeave={toggleSubMenu}>
+            <Link href="#" className={styles.link}>O evento</Link>
+            {isOpenLink && (
+              <div className={styles.submenu}>
                 <div>
-                  <div>
-                    <Link href="/pages/evento/comite">Comitê</Link>
-                    <Link href="/pages/evento/programacao">Programação</Link>
-                    <Link href="/pages/evento/cartaz">Cartaz</Link>
-                  </div>
+                  <Link href="/pages/evento/comite">Comitê</Link>
+                  <Link href="/pages/evento/programacao">Programação</Link>
+                  <Link href="/pages/evento/cartaz">Cartaz</Link>
                 </div>
-                </>
-              )
-            }
+              </div>
+            )}
           </li>
           <li><Link href="/pages/cronograma" className={styles.link}>Cronograma</Link></li>
           <li><Link href="/pages/posteres" className={styles.link}>Pôsteres</Link></li>
@@ -67,7 +64,7 @@ const Header = () => {
         </ul>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
